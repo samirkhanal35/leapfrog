f445206ad2367ddf294ec49457b0aecba01accb0
samirkhanal35 <samirkhanal35@gmail.com>
2019-11-28 20:40:49 +0545

    variable box width height and number

--- js/ast_3_box_collision/js/box_collision.js
+++ js/ast_3_box_collision/js/box_collision.js
@@ -7,27 +7,20 @@ var ball = [];
 
 
 
-function Box(parentElement, container_width, container_height, box_count) {
-    this.width = 40;
-    this.height = 40;
+function Box(parentElement, container_width, container_height, box_count, width, height) {
+    this.width = width;
+    this.height = height;
     this.box_count = box_count;
     this.boxes = [];
     this.x = [];
     this.y = [];
     this.container_width = container_width;
     this.container_height = container_height;
-    //  console.log(container_width);
-
-    //  console.log(typeof(this.x));
-    //  console.log(this.x);
     this.dx = [];
     this.dy = [];
-
     this.box = [];
     this.parentElement = parentElement;
     var that = this;
-
-
 
     this.init = function() {
         for (var i = 0; i < this.box_count; i++) {
@@ -38,49 +31,34 @@ function Box(parentElement, container_width, container_height, box_count) {
             this.parentElement.appendChild(box1);
             this.boxes.push(box1);
         }
-        //  this.element.onclick = this.boxClicked.bind(this);
-        //  this.draw();
-        //  console.log("entered init");
-        this.move_ball();
-        //  this.ball_access();
 
+        this.move_ball();
     }
 
-    //  this.ball_access() {
-
-    //  }
 
     this.move_ball = function() {
-        //  if (this.x < (this.width / 2) || this.x > (container_width - (this.width / 2))) {
-        //      this.dx = -this.dx;
-
-        //  }
 
         for (var i = 0; i < this.box_count; i++) {
 
-            //  console.log("entered move_ball initialization");
             this.box[i] = this.boxes[i];
-
-
-            this.dx[i] = Math.floor(Math.random() * 4) + 2;
-
+            this.dx[i] = Math.floor(Math.random() * 4) + 1;
             this.dy[i] = this.dx[i];
 
         }
         this.unique_entry();
 
-        setInterval(this.draw, 16);
+        setInterval(this.draw, 15);
 
     }
 
     this.unique_entry = function() {
-        //  console.log("inside unique");
+
         for (var i = 0; i < this.box_count; i++) {
-            //  console.log("inside loop");
-            this.x[i] = Math.floor(Math.random() * (container_width - (this.width + 45)));
-            //  console.log("x>>", this.x[i]);
-            this.y[i] = Math.floor(Math.random() * (container_height - (this.height + 45)));
-            //  console.log("y>>", this.y[i]);
+
+            this.x[i] = Math.floor(Math.random() * (container_width - (2 * this.width)));
+
+            this.y[i] = Math.floor(Math.random() * (container_height - (2 * this.height)));
+
             if (i > 0) {
                 for (var j = 0; j < i; j++) {
                     if (this.x[i] < this.x[j] + this.width &&
@@ -88,13 +66,8 @@ function Box(parentElement, container_width, container_height, box_count) {
                         this.y[i] < this.y[j] + this.height &&
                         this.y[i] + this.height > this.y[j]) {
                         this.unique_entry();
-
                     }
-
-
                 }
-
-
             }
         }
     }
@@ -103,17 +76,13 @@ function Box(parentElement, container_width, container_height, box_count) {
 
 
     this.draw = function() {
-
         for (var i = 0; i < that.box_count; i++) {
-            if (that.x[i] < 5 || that.x[i] > (container_width - (that.width + 5))) {
+            if (that.x[i] <= 5 || that.x[i] >= (container_width - (that.width + 5))) {
                 that.dx[i] = -that.dx[i];
-                //  console.log(that.x[i], that.width);
             }
-            if (that.y[i] < 5 || that.y[i] > (container_height - (that.height + 5))) {
+            if (that.y[i] <= 5 || that.y[i] >= (container_height - (that.height + 5))) {
                 that.dy[i] = -that.dy[i];
-                //  console.log("dx changed");
             }
-
             for (var j = 0; j < that.box_count; j++) {
                 if (j != i) {
                     if (that.x[i] < that.x[j] + that.width &&
@@ -125,24 +94,19 @@ function Box(parentElement, container_width, container_height, box_count) {
                         that.dy[i] = -that.dy[i];
                         that.dy[j] = -that.dy[j];
                     }
-
                 }
-
-
             }
-
-            //  console.log("entered draw initialization");
             that.box[i].style.left = that.x[i] + 'px';
             that.box[i].style.top = that.y[i] + 'px';
             that.x[i] += that.dx[i];
             that.y[i] += that.dy[i];
         }
-
-
     }
 
 }
 
 var parentElement = document.getElementById('ball_collision_container');
-
-ball = new Box(parentElement, container_width, container_height, 10).init();
+var box_width = 20;
+var box_height = 20;
+var number_of_boxes = 20;
+start_collision = new Box(parentElement, container_width, container_height, number_of_boxes, box_width, box_height).init();
