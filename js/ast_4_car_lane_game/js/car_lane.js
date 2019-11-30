1a2856cbf5db5bf780504c94f79652c32013a8be
samirkhanal35 <samirkhanal35@gmail.com>
2019-12-01 00:52:01 +0545

    Background and Car movement completed

--- js/ast_4_car_lane_game/js/car_lane.js
+++ js/ast_4_car_lane_game/js/car_lane.js
@@ -11,10 +11,30 @@ function car_lane_game(parentElement, container_height, container_width, backgro
     this.height = container_height;
     // console.log("height>>", this.height);
     this.width = container_width;
+    this.background_images = background_images;
+    console.log(this.background_images[0]);
+    this.image_height = this.background_images[0].clientHeight;
+    console.log("image height>>", this.image_height);
+    var that = this;
+    this.car_speed = 5;
+    this.image_index = 0;
+    this.car_position = 1;
 
     this.init = function() {
+        // Array.from(this.background_images).forEach(function(image, index) {
+        //     // console.log(image, index);
+        //     var margin = -(index * that.image_height);
+        //     // console.log(margin);
+        //     image.style.marginTop = -(index * that.image_height) + "px";
+        //     // console.log("margin top>>", image.style.marginTop);
+        // });
+        this.image_index = -(this.image_height - this.height);
+        this.background_images[0].style.marginTop = this.image_index + "px";
+        console.log(this.background_images[0].style.marginTop);
+        // console.log(that.background_images[0]);
         this.starting();
         // background_container
+
 
     }
 
@@ -43,9 +63,104 @@ function car_lane_game(parentElement, container_height, container_width, backgro
         start_box_parent.appendChild(start_box);
         start_image.addEventListener("click", function() {
             start_box.style.display = "none";
-            return;
+            that.start_playing_game();
         })
 
+
+    }
+
+    this.start_playing_game = function() {
+        var main_car_parent = document.getElementsByClassName("main_container")[0];
+        var main_car_box = document.createElement('div');
+
+        main_car_box.style.height = (this.height / 5) + "px";
+
+        main_car_box.style.width = (this.width / 5) + "px";
+
+        main_car_box.style.top = (this.height - (this.height / 4.5)) + "px";
+
+
+        main_car_box.style.left = (this.width / 2.6) + "px";
+
+        main_car_box.classList.add("main_car_box");
+        var main_car_image = document.createElement('img');
+        main_car_image.src = "./images/main_car.png";
+
+        main_car_box.appendChild(main_car_image);
+        main_car_parent.appendChild(main_car_box);
+
+
+
+        window.addEventListener("keydown", function(event) {
+            if (event.keyCode == '39') {
+                // right arrow key
+                if (that.car_position == 1) {
+                    main_car_box.style.left = (that.width / 1.3846) + "px";
+                    that.car_position = 2;
+                }
+                if (that.car_position == 0) {
+                    main_car_box.style.left = (that.width / 2.6) + "px";
+                    that.car_position = 1;
+                }
+
+                if (that.car_position == 2) {
+                    main_car_box.style.left = (that.width / 1.3846) + "px";
+                    that.car_position = 2;
+                }
+
+
+                // console.log("right>>", (that.width / 1.5574));
+            }
+            if (event.keyCode == '37') {
+                //left arrow key
+                if (that.car_position == 1) {
+                    main_car_box.style.left = (that.width / 13.50357) + "px";
+                    that.car_position = 0;
+                }
+                if (that.car_position == 2) {
+                    main_car_box.style.left = (that.width / 2.6) + "px";
+                    that.car_position = 1;
+                }
+                if (that.car_position == 0) {
+                    main_car_box.style.left = (that.width / 13.50357) + "px";
+                    that.car_position = 0;
+                }
+
+            }
+        });
+
+        // main_car_box.onkeydown = function(event) {
+        //     if (event.keyCode == '39') {
+        //         console.log("right");
+        //         main_car_box.style.left = (this.width / 3.8) + "px";
+        //     }
+        //     if (event.keyCode == '37') { main_car_box.style.left = (this.width / 1.8) + "px"; }
+
+        // }
+
+
+
+
+
+        setInterval(this.play_game, 30);
+
+
+    }
+
+    this.play_game = function() {
+        that.background_movement();
+
+
+    }
+
+
+    this.background_movement = function() {
+        if (that.image_index >= -that.car_speed) {
+            that.image_index = -(that.image_height - that.height);
+            that.car_speed += 1;
+        }
+        that.image_index += that.car_speed;
+        that.background_images[0].style.marginTop = that.image_index + "px";
 
     }
 
